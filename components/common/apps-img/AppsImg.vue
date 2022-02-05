ec8eae7a42f6e7e19fa8973577534de200406092
Jamal Khalili <j.khalili15@yahoo.com>
2022-02-06 03:57:12 +0400

    feat: Added hero and mixed media blocks

---
+++ components/common/apps-img/AppsImg.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="b-apps-img" >
+        <img
+            :src="imgSrc"
+            :alt="alt"
+        />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppsImg',
+    props: {
+        imgName: { 
+            type: String, 
+            default: ''
+        },
+        alt: { 
+            type: String, 
+            default: 'Image'
+        },
+    },
+    computed: {
+        imgSrc () {
+            const fileName = this.imgName.toLowerCase();
+
+            return require(`~/assets/img/${fileName}.jpg`) // the module request
+        },
+    },
+}
+</script>
+
+<style lang="scss">
+    .b-apps-img {
+        display: flex;
+        width: 100%;
+        height: 100%;
+        align-items: center;
+        justify-content: center;
+
+        img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+    }     
+</style>
